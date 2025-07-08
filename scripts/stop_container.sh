e64200618d35d8305290c37fa883023125810885
sravya bolla <160156727+iam-bolla@users.noreply.github.com>
2025-07-08 19:47:35 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-containerid='docker ps|awk -F""'{print $1}''
-docker rm -f '$containerid'
+containerid=$(docker ps | awk 'NR==2 {print $1}')
+docker rm -f "$containerid"
 
